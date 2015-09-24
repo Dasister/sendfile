661e03944e5c373470f91563769804074261954e
Dasister <maxsyrman@yandex.ru>
2015-09-24 19:26:16 +0300

    Initial commit

---
+++ client/src/main.cpp
@@ -0,0 +1,176 @@
+/*
+_________________¶¶¶¶¶¶¶¶¶¶¶
+______________¶¶¶¶¶¶¶____¶¶¶¶¶
+____________¶¶¶_¶¶__________¶¶¶
+___________¶¶___¶¶¶¶¶_________¶¶
+___________¶____¶¶¶¶¶¶_¶¶__¶___¶¶¶
+___________¶_¶¶_¶¶¶¶_¶¶¶¶¶___¶___¶¶
+___________¶¶¶¶_¶¶¶¶¶¶¶__¶¶¶¶_¶___¶
+___________¶¶¶¶_¶¶____¶¶____¶¶_¶¶_¶¶___¶¶¶¶
+____________¶¶¶¶¶_¶¶¶__¶_¶¶¶_¶¶_¶¶_¶¶_¶¶¶¶¶
+_____________¶¶¶¶_¶¶¶____¶¶¶_¶¶_¶¶¶¶¶¶¶¶¶¶
+______________¶¶¶¶_____¶¶____¶¶_¶¶¶¶__¶¶¶
+_____________¶¶_¶¶____¶¶____¶_¶¶¶¶__¶¶_¶¶
+____________¶¶¶_¶¶¶¶¶_____¶¶¶__¶__¶___¶¶
+___________¶¶¶_¶¶_¶¶¶¶___¶¶¶__¶¶¶_¶¶_¶¶
+____________¶¶¶¶¶_¶¶¶¶___¶_¶__¶¶¶¶__¶¶
+___________¶¶¶¶¶___¶¶¶¶__¶¶¶_¶¶¶_¶¶__¶¶
+__________¶¶¶_________¶¶__¶¶¶_¶__¶¶¶¶¶¶¶¶
+_______¶¶¶¶____________¶¶¶_¶¶¶____¶¶¶¶¶¶¶
+_____¶¶¶¶_______¶________¶¶¶_¶¶¶¶__¶___¶¶
+___¶¶¶¶______¶¶¶¶¶¶¶¶______¶¶_¶¶¶¶¶¶____¶¶
+__¶¶_______¶¶¶¶__¶¶¶¶__________¶¶¶_¶¶¶¶¶¶¶¶
+¶¶_______¶¶¶¶_¶__¶¶_¶___________¶¶_¶¶¶____¶¶
+¶____¶¶¶¶¶_____¶__¶¶__________¶__¶__¶¶_____¶
+¶___¶¶¶¶¶¶¶¶___¶¶__¶__________¶¶¶¶¶________¶
+¶¶¶_____¶__¶¶¶¶_¶¶___________¶¶_¶¶¶¶¶¶¶¶¶¶¶¶
+__¶¶¶¶¶¶______¶¶¶¶¶¶_______¶¶_¶¶
+____¶¶¶¶¶¶¶¶¶_____¶¶¶¶_____¶_¶¶
+_________¶¶¶¶¶¶¶¶¶¶__¶¶_¶____¶
+________________¶¶¶___¶¶¶____¶
+_______________¶¶____¶_¶¶¶¶¶¶¶
+____________¶¶¶¶¶¶__¶¶¶¶___¶¶
+_________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+________¶¶¶¶¶¶¶¶¶¶_¶_¶¶¶¶_¶¶¶
+_______¶¶¶¶¶_¶¶¶¶__¶¶_¶_¶¶_¶¶¶
+______¶¶¶¶¶_¶¶_¶¶___¶__¶¶¶¶_¶¶¶
+_____¶¶¶¶¶__¶__¶¶¶__¶¶__¶¶_¶¶¶¶¶
+____¶¶¶¶¶___¶__¶¶¶___¶___¶¶¶¶¶¶¶¶
+___¶¶¶¶¶___¶¶__¶¶¶___¶¶___¶¶¶_¶¶¶¶
+__¶¶¶¶¶¶___¶¶__¶¶¶¶___¶¶____¶¶_¶¶¶¶
+__¶¶¶¶¶___¶¶¶__¶¶_¶____¶¶¶___¶¶__¶¶¶
+_¶¶¶¶¶¶___¶¶¶___¶__¶____¶¶¶___¶¶¶_¶¶¶¶
+_¶¶¶_¶____¶¶¶___¶¶_¶¶____¶¶¶____¶¶__¶¶
+_¶¶_¶¶___¶¶¶¶____¶________¶¶¶____¶¶¶¶
+___¶¶____¶¶¶¶____¶¶________¶¶¶___¶¶¶
+___¶¶¶______¶_____¶_________¶¶¶¶¶¶
+____¶¶¶¶¶¶¶¶¶_____¶¶_¶_____¶¶¶
+________¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+________¶¶__¶¶¶¶¶¶¶_____¶¶
+________¶¶__¶¶__________¶
+_________¶___¶_________¶¶
+_________¶¶__¶_________¶
+_________¶¶__¶¶________¶
+_________¶¶__¶¶_______¶¶
+__________¶___¶_______¶¶
+__________¶¶__¶¶____¶¶¶
+___________¶__¶¶______¶
+__________¶¶__¶¶_____¶¶
+__________¶¶__¶______¶
+__________¶__¶¶______¶
+_________¶¶__¶_______¶
+_________¶¶_¶¶_______¶
+_________¶¶_¶¶_______¶
+_________¶¶_¶¶______¶¶
+_________¶¶_¶¶______¶¶
+__________¶¶_¶______¶¶
+___________¶_¶______¶¶
+___________¶¶¶¶¶¶¶¶¶¶¶
+____________¶_¶¶¶___¶¶
+____________¶¶¶¶¶___¶¶
+_____________¶¶¶____¶¶
+____________¶¶¶¶¶¶¶¶¶¶
+___________¶¶_¶¶¶¶¶_¶¶
+___________¶_¶¶¶¶__¶_¶¶
+___________¶¶¶______¶¶¶
+____________¶¶¶_______¶
+______________¶¶¶_____¶¶
+_______________¶_¶¶___¶¶
+_______________¶__¶¶___¶¶¶¶¶
+_______________¶¶¶¶¶____¶_¶¶¶
+___________________¶¶¶¶¶¶¶¶
+*/
+
+
+#include <cstdio>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <errno.h>
+#include <libgen.h>
+#include <sys/sendfile.h>
+#include <unistd.h>
+
+#define SERVER_PORT 50000
+#define SERVER_ADDR "localhost"
+
+int main(int argc, char* argv[]) {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    int fd;
+    ssize_t len;
+    int sent_bytes;
+    char file_size[256];
+    struct stat file_stat;
+    off_t offset;
+    int remain_data;
+    char filepath[255];
+
+    fprintf(stdout, "Input a valid path to file: ");
+    fgets(filepath, 255, stdin);
+    filepath[strlen(filepath) - 1] = '\0';
+    char *filename = basename(filepath);
+    fprintf(stdout, "Basename: %s\n", filename);
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        fprintf(stderr, "Error constructing socket: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, SERVER_ADDR, &(server_addr.sin_addr));
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
+        fprintf(stderr, "Can't connect: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Can't open file \"%s\" error: \"%s\"", filepath, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    if (fstat(fd, &file_stat) < 0) {
+        fprintf(stderr, "fstat failed: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stdout, "Sending filename: %s\n", filename);
+    len = send(client_socket, filename, strlen(filename), 0);
+    if (len < 0) {
+        fprintf(stderr, "failed to send filename: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    usleep(100);
+
+    sprintf(file_size, "%d", (int)file_stat.st_size);
+
+    len = send(client_socket, file_size, sizeof(file_size), 0);
+    if (len < 0) {
+        fprintf(stderr, "Failed to send file_size: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+    usleep(100);
+
+    offset = 0;
+    remain_data = (int)file_stat.st_size;
+
+    while (((sent_bytes = (int)sendfile(client_socket, fd, &offset, BUFSIZ)) > 0) && (remain_data > 0)) {
+        remain_data -= sent_bytes;
+    }
+
+    close(fd);
+    close(client_socket);
+
+    return 0;
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,166 @@
+/*
+_________________¶¶¶¶¶¶¶¶¶¶¶
+______________¶¶¶¶¶¶¶____¶¶¶¶¶
+____________¶¶¶_¶¶__________¶¶¶
+___________¶¶___¶¶¶¶¶_________¶¶
+___________¶____¶¶¶¶¶¶_¶¶__¶___¶¶¶
+___________¶_¶¶_¶¶¶¶_¶¶¶¶¶___¶___¶¶
+___________¶¶¶¶_¶¶¶¶¶¶¶__¶¶¶¶_¶___¶
+___________¶¶¶¶_¶¶____¶¶____¶¶_¶¶_¶¶___¶¶¶¶
+____________¶¶¶¶¶_¶¶¶__¶_¶¶¶_¶¶_¶¶_¶¶_¶¶¶¶¶
+_____________¶¶¶¶_¶¶¶____¶¶¶_¶¶_¶¶¶¶¶¶¶¶¶¶
+______________¶¶¶¶_____¶¶____¶¶_¶¶¶¶__¶¶¶
+_____________¶¶_¶¶____¶¶____¶_¶¶¶¶__¶¶_¶¶
+____________¶¶¶_¶¶¶¶¶_____¶¶¶__¶__¶___¶¶
+___________¶¶¶_¶¶_¶¶¶¶___¶¶¶__¶¶¶_¶¶_¶¶
+____________¶¶¶¶¶_¶¶¶¶___¶_¶__¶¶¶¶__¶¶
+___________¶¶¶¶¶___¶¶¶¶__¶¶¶_¶¶¶_¶¶__¶¶
+__________¶¶¶_________¶¶__¶¶¶_¶__¶¶¶¶¶¶¶¶
+_______¶¶¶¶____________¶¶¶_¶¶¶____¶¶¶¶¶¶¶
+_____¶¶¶¶_______¶________¶¶¶_¶¶¶¶__¶___¶¶
+___¶¶¶¶______¶¶¶¶¶¶¶¶______¶¶_¶¶¶¶¶¶____¶¶
+__¶¶_______¶¶¶¶__¶¶¶¶__________¶¶¶_¶¶¶¶¶¶¶¶
+¶¶_______¶¶¶¶_¶__¶¶_¶___________¶¶_¶¶¶____¶¶
+¶____¶¶¶¶¶_____¶__¶¶__________¶__¶__¶¶_____¶
+¶___¶¶¶¶¶¶¶¶___¶¶__¶__________¶¶¶¶¶________¶
+¶¶¶_____¶__¶¶¶¶_¶¶___________¶¶_¶¶¶¶¶¶¶¶¶¶¶¶
+__¶¶¶¶¶¶______¶¶¶¶¶¶_______¶¶_¶¶
+____¶¶¶¶¶¶¶¶¶_____¶¶¶¶_____¶_¶¶
+_________¶¶¶¶¶¶¶¶¶¶__¶¶_¶____¶
+________________¶¶¶___¶¶¶____¶
+_______________¶¶____¶_¶¶¶¶¶¶¶
+____________¶¶¶¶¶¶__¶¶¶¶___¶¶
+_________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+________¶¶¶¶¶¶¶¶¶¶_¶_¶¶¶¶_¶¶¶
+_______¶¶¶¶¶_¶¶¶¶__¶¶_¶_¶¶_¶¶¶
+______¶¶¶¶¶_¶¶_¶¶___¶__¶¶¶¶_¶¶¶
+_____¶¶¶¶¶__¶__¶¶¶__¶¶__¶¶_¶¶¶¶¶
+____¶¶¶¶¶___¶__¶¶¶___¶___¶¶¶¶¶¶¶¶
+___¶¶¶¶¶___¶¶__¶¶¶___¶¶___¶¶¶_¶¶¶¶
+__¶¶¶¶¶¶___¶¶__¶¶¶¶___¶¶____¶¶_¶¶¶¶
+__¶¶¶¶¶___¶¶¶__¶¶_¶____¶¶¶___¶¶__¶¶¶
+_¶¶¶¶¶¶___¶¶¶___¶__¶____¶¶¶___¶¶¶_¶¶¶¶
+_¶¶¶_¶____¶¶¶___¶¶_¶¶____¶¶¶____¶¶__¶¶
+_¶¶_¶¶___¶¶¶¶____¶________¶¶¶____¶¶¶¶
+___¶¶____¶¶¶¶____¶¶________¶¶¶___¶¶¶
+___¶¶¶______¶_____¶_________¶¶¶¶¶¶
+____¶¶¶¶¶¶¶¶¶_____¶¶_¶_____¶¶¶
+________¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+________¶¶__¶¶¶¶¶¶¶_____¶¶
+________¶¶__¶¶__________¶
+_________¶___¶_________¶¶
+_________¶¶__¶_________¶
+_________¶¶__¶¶________¶
+_________¶¶__¶¶_______¶¶
+__________¶___¶_______¶¶
+__________¶¶__¶¶____¶¶¶
+___________¶__¶¶______¶
+__________¶¶__¶¶_____¶¶
+__________¶¶__¶______¶
+__________¶__¶¶______¶
+_________¶¶__¶_______¶
+_________¶¶_¶¶_______¶
+_________¶¶_¶¶_______¶
+_________¶¶_¶¶______¶¶
+_________¶¶_¶¶______¶¶
+__________¶¶_¶______¶¶
+___________¶_¶______¶¶
+___________¶¶¶¶¶¶¶¶¶¶¶
+____________¶_¶¶¶___¶¶
+____________¶¶¶¶¶___¶¶
+_____________¶¶¶____¶¶
+____________¶¶¶¶¶¶¶¶¶¶
+___________¶¶_¶¶¶¶¶_¶¶
+___________¶_¶¶¶¶__¶_¶¶
+___________¶¶¶______¶¶¶
+____________¶¶¶_______¶
+______________¶¶¶_____¶¶
+_______________¶_¶¶___¶¶
+_______________¶__¶¶___¶¶¶¶¶
+_______________¶¶¶¶¶____¶_¶¶¶
+___________________¶¶¶¶¶¶¶¶
+*/
+
+
+#include <cstdio>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <errno.h>
+#include <unistd.h>
+
+#define SERVER_PORT 50000
+#define SERVER_ADDR "localhost"
+
+int main(int argc, char *argv[]) {
+    int server_socket;
+    int peer_socket;
+    socklen_t sock_len;
+    ssize_t len;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in peer_addr;
+    int file_size;
+    int remain_data;
+    char buffer[BUFSIZ];
+    char filename[255];
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        fprintf(stderr, "Error constructing socket: %s", strerror(errno));
+
+        exit(EXIT_FAILURE);
+    }
+
+    /* Zeroing server_addr struct */
+    memset(&server_addr, 0, sizeof(server_addr));
+    /* Construct server_addr struct */
+    server_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, SERVER_ADDR, &(server_addr.sin_addr));
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    /* Bind */
+    if ((bind(server_socket, (struct sockaddr *) &server_addr, sizeof(struct sockaddr))) == -1) {
+        fprintf(stderr, "Error on bind: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    /* Listening to incoming connections */
+    if ((listen(server_socket, 5)) == -1) {
+        fprintf(stderr, "Error on listen: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    sock_len = sizeof(struct sockaddr_in);
+    /* Accepting incoming peers */
+    peer_socket = accept(server_socket, (struct sockaddr *) &peer_addr, &sock_len);
+    if (peer_socket == -1) {
+        fprintf(stderr, "Error on accept: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+    recv(peer_socket, buffer, BUFSIZ, 0);
+    strcpy(filename, buffer);
+
+    recv(peer_socket, buffer, BUFSIZ, 0);
+    file_size = atoi(buffer);
+
+    fprintf(stdout, "Filename: \"%s\", Filesize: \"%d\"\n", filename, file_size);
+
+    FILE *received_file = fopen(filename, "wb");
+    if (received_file == NULL) {
+        fprintf(stderr, "Can't create file \"%s\" with error \"%s\"", filename, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    remain_data = file_size;
+    while (((len = recv(peer_socket, buffer, BUFSIZ, 0)) > 0) && (remain_data > 0)) {
+        fwrite(buffer, sizeof(char), len, received_file);
+        remain_data -= len;
+    }
+    fclose(received_file);
+    close(peer_socket);
+    close(server_socket);
+
+    return 0;
+}
